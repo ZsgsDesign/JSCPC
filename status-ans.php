3b2626f841e44b3bbdb9c38d3c8dda3d0d394d9c
ZsgsDesign <zsgsdesign@gmail.com>
2019-04-17 14:35:39 +0800

    update

---
+++ status-ans.php
@@ -0,0 +1,20 @@
+<?php
+header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+
+////////////////////////////Common head
+        $cache_time=2;
+        $OJ_CACHE_SHARE=false;
+    require_once('./include/db_info.inc.php');
+        require_once('./include/setlang.php');
+        $view_title= "$MSG_STATUS";
+
+
+require_once("./include/const.inc.php");
+
+if (isset($_GET['sid'])) {
+    $sid=intval($_GET['sid']);
+    $sql_lock="SELECT * FROM `solution` LEFT JOIN `source_code` ON `solution`.`solution_id`=`source_code`.`solution_id` WHERE `solution_id`=?";
+    $result=pdo_query($sql_lock, $cid) ;
+    var_dump($result[0]);
+}
