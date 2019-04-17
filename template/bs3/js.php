ecba9f332a50044ba06daed9c8c01ba6340e1a80
ZsgsDesign <zsgsdesign@gmail.com>
2019-04-17 14:17:05 +0800

    enable mathjax

--- template/bs3/js.php
+++ template/bs3/js.php
@@ -12,17 +12,11 @@ $view_marquee_msg=file_get_contents($OJ_SAE?"saestor://web/msg.txt":"../admin/ms
 
 
 ?>
-<!--  to enable mathjax in hustoj:
-svn export http://github.com/mathjax/MathJax/trunk /home/judge/src/web/mathjax
+
 <script type="text/javascript"
   src="mathjax/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
 </script>
-or
-<script type="text/javascript"
-  src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
-</script>
 
--->
 <script>
 $(document).ready(function(){
   var msg="<marquee style='margin-top:10px' id=broadcast direction='left' scrollamount=3 scrolldelay=50 onMouseOver='this.stop()'"+
@@ -43,9 +37,6 @@ $(".hint pre").each(function(){
 	});
 	
 });
-
-  console.log("If you want to change the appearance of the web pages, make a copy of bs3 under template directory.\nRename it to whatever you like, and change the $OJ_TEMPLATE value in db_info.inc.php\nAfter that modify files under your own directory .\n");
-  console.log("To enable mathjax in hustoj, check line 15 in /home/judge/src/web/template/bs3/js.php");
 
 </script>
 
